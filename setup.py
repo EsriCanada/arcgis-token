0a7a6c80dc4de2be6b1e82004a16b068201f2d3c
Adam <amarinelli@esri.ca>
2015-01-16 10:29:49 -0500

    Modified setup.py
    - Build successful using py2exe

--- setup.py
+++ setup.py
@@ -5,7 +5,9 @@ setup(windows=["arcgis_token.py"],
       options=dict(
           py2exe={
               "includes":
-                  ["PyQt4.QtGui", "PyQt4.QtCore"],
+                  ["PyQt4.QtGui", "PyQt4.QtCore", "sip"],
               "dll_excludes":
                   ["MSVCP90.dll"]
           }))
+
+# C:\Python27\Python279\python.exe setup.py py2exe
