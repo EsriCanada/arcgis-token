7d1acc636834dc818298d7cc44f609358c597df5
Adam <amarinelli@esri.ca>
2015-01-15 20:05:04 -0500

    Modify setup.py to fix error
    - error: [Errno 2] No such file or directory: 'MSVCP90.dll'

--- setup.py
+++ setup.py
@@ -1,4 +1,11 @@
 from distutils.core import setup
 import py2exe
 
-setup(windows=['arcgis_token.py'], options={"py2exe": {"includes": ["PyQt4.QtGui", "PyQt4.QtCore"]}})
+setup(windows=["arcgis_token.py"],
+      options=dict(
+          py2exe={
+              "includes":
+                  ["PyQt4.QtGui", "PyQt4.QtCore"],
+              "dll_excludes":
+                  ["MSVCP90.dll"]
+          }))
