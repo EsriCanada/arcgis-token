7f472f3f38c7bf9a4af3b3f9912481024fd68f2f
Adam <amarinelli@esri.ca>
2015-01-16 14:03:59 -0500

    Formatting imports and added __name__

--- arcgis_token.py
+++ arcgis_token.py
@@ -4,16 +4,16 @@ import urllib
 import time
 import ctypes
 
-from PyQt4.QtCore import *
-from PyQt4.QtGui import *
+import PyQt4.QtCore as QtCore
+import PyQt4.QtGui as QtGui
 
 
-class ArcGIS(QDialog):
+class ArcGIS(QtGui.QDialog):
     def __init__(self):
-        QDialog.__init__(self)
-        layout = QVBoxLayout()
+        QtGui.QDialog.__init__(self)
+        layout = QtGui.QVBoxLayout()
 
-        self.clipboard = QApplication.clipboard()
+        self.clipboard = QtGui.QApplication.clipboard()
 
         # Variables
         self.main_title = "ArcGIS Online Token Generator"
@@ -22,21 +22,21 @@ class ArcGIS(QDialog):
         self.expirations = ['60', '120', '180', 'max']
 
         # Design widgets
-        self.title_lbl = QLabel(self.main_title)
-        self.username_txt = QLineEdit()
-        self.password_txt = QLineEdit()
-        self.expiration_combo = QComboBox()
-        self.clipboard_chk = QCheckBox("Copy token to clipboard ?")
-        self.get_token_btn = QPushButton("Get Token")
-        self.message_lbl = QLabel()
-        self.sub_message_lbl = QLabel()
-        self.expiration_lbl = QLabel()
-        self.token_output = QTextEdit()
+        self.title_lbl = QtGui.QLabel(self.main_title)
+        self.username_txt = QtGui.QLineEdit()
+        self.password_txt = QtGui.QLineEdit()
+        self.expiration_combo = QtGui.QComboBox()
+        self.clipboard_chk = QtGui.QCheckBox("Copy token to clipboard ?")
+        self.get_token_btn = QtGui.QPushButton("Get Token")
+        self.message_lbl = QtGui.QLabel()
+        self.sub_message_lbl = QtGui.QLabel()
+        self.expiration_lbl = QtGui.QLabel()
+        self.token_output = QtGui.QTextEdit()
 
         # Configure
         self.username_txt.setPlaceholderText("Username")
         self.password_txt.setPlaceholderText("Password")
-        self.password_txt.setEchoMode(QLineEdit.Password)
+        self.password_txt.setEchoMode(QtGui.QLineEdit.Password)
         self.get_token_btn.clicked.connect(self.get_token)
         self.expiration_combo.addItems(self.expirations)
 
@@ -53,25 +53,25 @@ class ArcGIS(QDialog):
         layout.addWidget(self.token_output)
 
         # Formatting
-        layout.setAlignment(self.title_lbl, Qt.AlignCenter)
-        layout.setAlignment(self.username_txt, Qt.AlignCenter)
-        layout.setAlignment(self.password_txt, Qt.AlignCenter)
-        layout.setAlignment(self.expiration_combo, Qt.AlignCenter)
-        layout.setAlignment(self.get_token_btn, Qt.AlignCenter)
-        layout.setAlignment(self.clipboard_chk, Qt.AlignCenter)
-        layout.setAlignment(self.message_lbl, Qt.AlignCenter)
-        layout.setAlignment(self.sub_message_lbl, Qt.AlignCenter)
+        layout.setAlignment(self.title_lbl, QtCore.Qt.AlignCenter)
+        layout.setAlignment(self.username_txt, QtCore.Qt.AlignCenter)
+        layout.setAlignment(self.password_txt, QtCore.Qt.AlignCenter)
+        layout.setAlignment(self.expiration_combo, QtCore.Qt.AlignCenter)
+        layout.setAlignment(self.get_token_btn, QtCore.Qt.AlignCenter)
+        layout.setAlignment(self.clipboard_chk, QtCore.Qt.AlignCenter)
+        layout.setAlignment(self.message_lbl, QtCore.Qt.AlignCenter)
+        layout.setAlignment(self.sub_message_lbl, QtCore.Qt.AlignCenter)
 
-        self.palette = QPalette()
+        self.palette = QtGui.QPalette()
 
-        self.sub_expiration_message_font = QFont()
+        self.sub_expiration_message_font = QtGui.QFont()
         self.sub_expiration_message_font.setPointSize(7)
         self.sub_message_lbl.setFont(self.sub_expiration_message_font)
 
-        self.expiration_font = QFont()
+        self.expiration_font = QtGui.QFont()
         self.expiration_font.setBold(True)
         self.expiration_lbl.setFont(self.expiration_font)
-        layout.setAlignment(self.expiration_lbl, Qt.AlignCenter)
+        layout.setAlignment(self.expiration_lbl, QtCore.Qt.AlignCenter)
 
         self.setLayout(layout)
         self.setWindowTitle(self.main_title)
@@ -91,14 +91,14 @@ class ArcGIS(QDialog):
         submitJson = json.loads(submitResponse.read())
 
         if 'error' in submitJson:
-            self.palette.setColor(QPalette.Foreground, Qt.red)
+            self.palette.setColor(QtGui.QPalette.Foreground, QtCore.Qt.red)
             self.message_lbl.setPalette(self.palette)
             self.message_lbl.setText(str(submitJson['error']['details'][0]))
         else:
             if self.clipboard_chk.isChecked():
                 self.clipboard.clear()
                 self.clipboard.setText(str(submitJson['token']), mode=self.clipboard.Clipboard)
-            self.palette.setColor(QPalette.Foreground, Qt.blue)
+            self.palette.setColor(QtGui.QPalette.Foreground, QtCore.Qt.blue)
             self.message_lbl.setPalette(self.palette)
             self.message_lbl.setText("Token Granted")
             self.sub_message_lbl.setText("Your token expires:")
@@ -107,15 +107,17 @@ class ArcGIS(QDialog):
             self.token_output.insertPlainText(str(submitJson['token']))
 
 
-app = QApplication(sys.argv)
+if __name__ == "__main__":
 
-myappid = u'esricanada.tokengenerator.arcgis.1'
-ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(myappid)
+    app = QtGui.QApplication(sys.argv)
 
-dialog = ArcGIS()
-dialog.setFixedSize(200, 325)
-dialog.setWindowIcon(QIcon('icon.ico'))
-app.setWindowIcon(QIcon('icon.ico'))
+    appid = u'esricanada.tokengenerator.arcgis.0.1'
+    ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(appid)
 
-dialog.show()
-app.exec_()
+    dialog = ArcGIS()
+    dialog.setFixedSize(200, 325)
+    dialog.setWindowIcon(QtGui.QIcon('icon.ico'))
+    app.setWindowIcon(QtGui.QIcon('icon.ico'))
+
+    dialog.show()
+    sys.exit(app.exec_())
